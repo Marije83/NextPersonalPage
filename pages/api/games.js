b61fccaad1c7dfa09334e4c62f7f5d15b1bb7810
Marije83 <marije.vanhattum@hotmail.nl>
2023-02-07 15:04:14 +0000

    2nd attempt to change hard-code to API

--- pages/api/games.js
+++ pages/api/games.js
@@ -3,21 +3,25 @@ export default function handler(req, res){
         {
             title: "The Last of us",
             //picture: <img src='lastOfUs.jfif' alt="The Last of Us" width="150" height ="300" />,
+            picture: "/assets/lastOfUs.jfif",
             developer: "Naughty Dog",
         },
         {
             title: "Final Fantasy XII",
             //picture: <img src='FFXII.jfif' alt="Final Fantasy XII" width="150" height ="300" />,
+            picture: "/assets/FFXII.jfif",
             developer: "Square Enix",
         },
         {
             title: "Overcooked",
             //picture: <img src='Overcooked.jpg' alt="Overcooked" width="150" height ="300"/>,
+            picture: "/assets/Overcooked.jpg",
             developer:"Ghost Town Games"
         },
         {
             title: "Brothers: A Tale of Two Sons",
             //picture: <img src='Brothers.jfif' alt="Brothers" width="150" height ="300"/>,
+            picture: "/assets/Brothers.jfif",
             developer: "Starbreeze Studios"
         }
     
