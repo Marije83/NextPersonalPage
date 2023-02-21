cc9c7bbd87d1dfb9b12a742f51547783794636f9
Marije83 <marije.vanhattum@hotmail.nl>
2023-02-21 21:10:19 +0000

    responsive design for home and video poll

--- components/video-game-poll-grid/index.js
+++ components/video-game-poll-grid/index.js
@@ -53,7 +53,7 @@ export default function GamePoll () {
     return(
         <div>
             <div
-            className = "grid grid-cols-4 shrink gap-y-10 text-center bg-slate-100 m-5 p-3">
+            className = "grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 shrink gap-y-10 text-center bg-slate-100 m-5 p-3">
            {games.map((game, index) => {
                 return (
                     <GameItem 
@@ -66,17 +66,17 @@ export default function GamePoll () {
 
             <div className = "text-center">
                 <p>Or you can add your own favourite game here!</p>
-                <form className = "m-5">
-                    <label className = "m-5">Title:</label>
-                    <input className = "border border-purple-900 hover:bg-purple-100"
+                <form className = "m-5 grid grid-row md:flex flex-cols-2 md: gap-4 md:justify-around">
+                    <input placeholder="title" className = "pl-5 border rounded h-10 w-full md:w-2/5 border-purple-900 hover:bg-purple-100"
                     value = {title}
                     onChange = {handleSetTitle}/>
-                    <label className = "m-5">Developer:</label>
-                    <input className = "border border-purple-900 hover:bg-purple-100"
+                    
+                    <input placeholder="developer" className = "pl-5 border h-10 w-full md:w-2/5 rounded border-purple-900 hover:bg-purple-100"
                     value = {developer}
                     onChange = {handleSetDeveloper}/>
+                    
                     <button
-                        className = "bg-purple-900 text-white text-center m-5 px-3 py-1 rounded"
+                        className = "bg-purple-900 text-white text-center px-3 w-full md:w-1/5 h-10 rounded"
                         type = "button"
                         onClick = {handleAddGame}
                         >Add Game</button>
