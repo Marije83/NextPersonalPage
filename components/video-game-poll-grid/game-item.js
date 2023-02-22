7c1e861c2e32cf6cbc20defe06e0ef956310cf7f
Marije83 <marije.vanhattum@hotmail.nl>
2023-02-22 13:53:51 +0000

    include temp image for form

--- components/video-game-poll-grid/game-item.js
+++ components/video-game-poll-grid/game-item.js
@@ -19,7 +19,9 @@ export default function GameItem ({game}) {
     return(
         <div>
             <Link href={`/single-game/${game.id}`} className = "font-bold">{game.title}</Link>
-            <div className= "grid justify-center h-52 items-center"><img src={game.picture} alt="The Last of Us" width="150" height ="300" /> </div>
+            <div className= "grid justify-center h-52 items-center">    
+                {game.picture ? (<img src={game.picture} alt="The Last of Us" width="150" height ="300"/>): (<img src="/assets/TBC.png" alt="TBC" width="150" height ="300"/>)}
+            </div>
             <div className = "italic">{game.developer}</div>
             <button
                 className= "bg-purple-900 text-white px-3 mt-3 rounded"
