dbc226368fe7d4beda1e68acb471f65b5c5e9cdc
Marije83 <marije.vanhattum@hotmail.nl>
2023-02-22 19:35:08 +0000

    changed order of games to by id

--- pages/api/games.js
+++ pages/api/games.js
@@ -4,7 +4,7 @@ export default async function handler(req, res){
 
     const games = await sql`
         select * from games
-        order by likes desc
+        order by id asc
     `;
 
     res.json(games);
