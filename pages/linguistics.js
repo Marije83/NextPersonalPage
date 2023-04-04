ddb91d833f156b4ac4477f9e46359db390338b23
Marije83 <marije.vanhattum@hotmail.nl>
2023-04-04 17:09:17 +0100

    Added the linguist page and updated navbar

---
+++ pages/linguistics.js
@@ -0,0 +1,17 @@
+import Page from "@/components/shared/page"
+import NavigationBar from "@/components/shared/navigation-bar"
+import Hero from "@/components/shared/hero"
+
+export default function Linguistics () {
+    return(
+        <Page>
+      
+            <NavigationBar></NavigationBar>
+      
+            <div>
+                <Hero name="THE LINGUIST"></Hero>
+            </div>
+          
+        </Page> 
+    )
+}
--- pages/video-game-poll.js
+++ pages/video-game-poll.js
@@ -4,7 +4,7 @@ import GamePoll from "@/components/video-game-poll-grid"
 import Page from "@/components/shared/page"
 import ContentText from "@/components/shared/content-text"
 
-export default function () {
+export default function Games () {
     
     return(
 
@@ -12,10 +12,10 @@ export default function () {
 
             <NavigationBar></NavigationBar>
             
-            <Hero name="Video games and other hobbies"></Hero>
+            <Hero name="THE GAMER"></Hero>
 
             <ContentText>
-                In my spare time I like to walk in the hills, read or wath TV, in particular fantasy and science fiction. I also enjoy playing video games. Some of my favourite games are <em>The Last of Us</em>, the <em>Final Fantasy</em> series, the <em>Uncharted</em> series, <em>Lego: Lord of the Rings</em>, <em>Overcooked</em> and <em>Brothers: A Tale of Two Sons</em>. What is your favourite game?
+                In my spare time I like to walk in the hills, read or wath TV, in particular fantasy and science fiction. And though I mainly created this page for to practice some coding principles, I do also enjoy playing video games. Some of my favourite games are <em>The Last of Us</em>, the <em>Final Fantasy</em> series, the <em>Uncharted</em> series, <em>Lego: Lord of the Rings</em>, <em>Overcooked</em> and <em>Brothers: A Tale of Two Sons</em>. What is your favourite game?
             </ContentText>
 
             <GamePoll></GamePoll>
